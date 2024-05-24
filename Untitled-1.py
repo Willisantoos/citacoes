387c94e93ff67330760b72665e32edb49bb16699
Willi Santoos <167312361+Willisantoos@users.noreply.github.com>
2024-05-24 08:49:41 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+def peso_ideal(alt, sexo):
+    if sexo == "M"
+    print(f"seu peso_ideal e {62.1 * alt -44.7:.1f}kg")
+else sexo =="M":
+print("erro! o programador sera fechado: ")
+
+altura=float(input("digite sua altura: "))
+sexo=str(input("sexo {F/M}: ")).strip().lower()
+peso_ideal(altura, sexo)
